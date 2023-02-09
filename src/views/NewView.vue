d87006dfccc35b3d602b7a837c4124a766091b0a
naka <nakasyo2792@gmail.com>
2023-02-09 22:24:55 +0900

    first commit

---
+++ src/views/NewView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>New</h2>
+  <div>
+    <input type="text" />
+  </div>
+  <div>
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+  </div>
+  <div class="center">
+    <button>保存</button>
+  </div>
+</template>
